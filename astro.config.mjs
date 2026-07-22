6fa5af187c373f67e19271ab1ba9fc72b4ece5a7
Ryodgie Barnatia <ryodgie@gmail.com>
2026-07-22 21:34:38 +0800

    feat: add BaseLayout with meta and OG tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://ryodgie.com",
   integrations: [sitemap()],
   fonts: [
     {
