6719fba70d6563be02eb9a2076e51fbffc38292c
Ryodgie Barnatia <ryodgie@gmail.com>
2026-07-28 13:12:36 +0800

    refactor: centralize email and social links in lib/site

---
+++ src/lib/site.ts
@@ -0,0 +1,7 @@
+export const EMAIL = "hello@ryodgie.com";
+
+export const SOCIALS = {
+  linkedin: "https://www.linkedin.com/in/ryodgie/",
+  dribbble: "https://dribbble.com/ryodgie",
+  github: "https://github.com/eigdoyr",
+};
