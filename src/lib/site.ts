2052c2936b101b1684a5ec9baee11abb5c82b7c1
Ryodgie Barnatia <ryodgie@gmail.com>
2026-07-29 03:11:57 +0800

    feat: add JSON-LD authorship, centralize identity in lib/site

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,16 @@
 export const EMAIL = "hello@ryodgie.com";
+export const NAME = "Ryodgie Barnatia";
+export const SITE_URL = "https://www.ryodgie.com";
+export const JOB_TITLE = "Front-End Web Designer & Developer";
 
 export const SOCIALS = {
   linkedin: "https://www.linkedin.com/in/ryodgie/",
   dribbble: "https://dribbble.com/ryodgie",
   github: "https://github.com/eigdoyr",
+};
+
+export const AUTHOR = {
+  "@type": "Person",
+  name: NAME,
+  url: SITE_URL,
 };
